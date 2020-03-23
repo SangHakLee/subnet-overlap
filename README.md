2d15f6d4ddb1289dc90ec36efe76f743f3bc5c36
leesanghak <leesanghak@gabia.com>
2020-03-24 08:51:11 +0900

    v0.1.0

---
+++ README.md
@@ -0,0 +1,5 @@
+# cidr-overlapped
+
+[![npm version](https://badge.fury.io/js/is-0.svg)](https://badge.fury.io/js/cidr-overlapped)
+
+[![NPM](https://nodei.co/npm/[![NPM](https://nodei.co/npm/cidr-overlapped.png)](https://nodei.co/npm/cidr-overlapped/).png?downloads=true&downloadRank=true&stars=true)](https://nodei.co/npm/[![NPM](https://nodei.co/npm/cidr-overlapped.png)](https://nodei.co/npm/cidr-overlapped/)/)
---
+++ test/index.spec.js
@@ -0,0 +1,29 @@
+const debug = require('debug')('test')
+const should = require('should')
+
+const cidrOverlapped = require('../index')
+
+const overlapped = Object.freeze([
+    [['172.22.2.0/24'], '172.22.2.0/24'],
+])
+
+const non_overlapped = Object.freeze([
+    [['172.22.1.0/24'], '172.22.2.0/24'],
+    [['172.22.1.0/24', '172.22.2.0/24'], '172.22.3.0/24'],
+])
+
+describe('cidrOverlapped', () => {
+    overlapped.forEach((v) => {
+        it(`Overlapped: (${JSON.stringify(v[0])}, ${v[1]})`, () => {
+            const r = cidrOverlapped(...v)
+            r.should.be.String()
+        })
+    })
+    
+    non_overlapped.forEach((v) => {
+        it(`Non-overlapped: (${JSON.stringify(v[0])}, ${v[1]})`, () => {
+            const r = cidrOverlapped(...v)
+            r.should.be.False()
+        })
+    })
+})
